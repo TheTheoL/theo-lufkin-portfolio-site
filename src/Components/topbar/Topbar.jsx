1a74ee753aa1962a9bb9d8590df04c8a102e94df
Theo L <atlufkin@outlook.com>
2022-04-14 14:43:34 -0400

    “updated”

--- src/Components/topbar/Topbar.jsx
+++ src/Components/topbar/Topbar.jsx
@@ -3,7 +3,19 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
+            {/* content that goes inside the toolbar */}
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Theo Lufkin</a>
+                    <div className="itemContainer">
 
+                    </div>
+                </div>
+
+                <div className="right">
+                    this is right
+                </div>
+            </div>
         </div>
     )
 }
