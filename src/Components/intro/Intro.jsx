945f13935f8df03392006d7a35945ba2535a3692
Theo L <atlufkin@outlook.com>
2022-04-15 13:48:56 -0400

    “updated”

--- src/Components/intro/Intro.jsx
+++ src/Components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="picture of handsome man"></img>
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Theo Lufkin</h1>
+                    <h3>Front-end  <span>Web Developer</span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="down errow" />
+                </a>
+            </div>
         </div>
     )
 }
