6d21b8760371e5249d23b898a8555dc6db271614
Theo L <atlufkin@outlook.com>
2022-04-18 15:28:48 -0400

    “updated”

---
+++ data.js
@@ -0,0 +1,23 @@
+export const featuredPortfolio = [{
+        id: 1,
+        title: "Interactive Registration Form",
+        img: "/assets/7-landing.png"
+    },
+    {
+        id: 1,
+        title: "Scoreboard",
+        img: "/assets/scoreboard.png"
+    },
+    {
+        id: 1,
+        title: "Interactive Registration Form",
+        img: "/assets/7-landing.png"
+    },
+    {
+        id: 1,
+        title: "Interactive Registration Form",
+        img: "/assets/7-landing.png"
+    },
+
+
+]
--- src/Components/portfolio/Portfolio.jsx
+++ src/Components/portfolio/Portfolio.jsx
@@ -1,9 +1,74 @@
+import PortfolioList from "../portpolioList/PortfolioList"
 import "./portfolio.scss"
+import { useEffect, useState } from 'react';
 
 export default function Portfolio() {
+
+    const [selected, setSelected] = useState("featured");
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured"
+        },
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "mobile",
+            title: "Mobile App"
+        },
+        {
+            id: "design",
+            title: "Design"
+        },
+        {
+            id: "branding",
+            title: "Branding"
+        },
+
+    ]
+
     return (
         <div className="portfolio" id="portfolio">
-
-        </div>
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item) => (
+                    <PortfolioList
+                        title={item.title}
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id}
+                    />
+                ))}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/7-landing.png" alt="" />
+                    <h3>Interactive Registration Form</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/scoreboard.png" alt="" />
+                    <h3>React Scoreboard</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/to-do.png" alt="" />
+                    <h3>Javascript to-do List</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/fullstack.png" alt="" />
+                    <h3>React Course Registration</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/7-landing.png" alt="" />
+                    <h3>Interactive Registration Form</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/7-landing.png" alt="" />
+                    <h3>Interactive Registration Form</h3>
+                </div>
+            </div>
+        </div >
     )
 }
